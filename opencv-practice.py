db13c090ea1fa072b759b3a6d8c643fa28cd6b02
GnotAI <alleluiabriggs@gmail.com>
2024-09-15 09:10:39 +0100

    opencv boredom project started

--- opencv-practice.py
+++ opencv-practice.py
@@ -1,1 +1,14 @@
-import cv2
+import cv2 as cv
+cam = cv.VideoCapture(0)
+result, image = cam.read()
+
+
+if result:
+
+  cv.imshow("Display", image)
+  # cv.imwrite("testing.png", image)
+  cv.waitKey(0)
+  cv.destroyWindow("Display")
+
+else:
+  print("No image was detected.")
